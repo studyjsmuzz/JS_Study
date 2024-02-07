79e0297d9a97578132680e7fa82401f0924b0af8
studyjsmuzz <158498431+studyjsmuzz@users.noreply.github.com>
2024-02-07 16:14:31 +0000

    first commit

---
+++ bgchanger/src/Test.jsx
@@ -0,0 +1,13 @@
+//import React from 'react'
+
+function Test() {
+  return (
+    <div>
+      <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+       onClick={()=> document.body.style.background="blue"} >Color</button>
+      
+    </div>
+  )
+}
+
+export default Test
